ae230ff4a2a41e61714869e970eb1d6d262821e9
HpyWorld <myqy480@gmail.com>
2019-02-13 17:34:12 +0800

    Create mystuff.py

---
+++ LearnPyTheHardWay/exer40/mystuff.py
@@ -0,0 +1,16 @@
+# This goes in mystuff.py
+def apple():
+    """A simple function"""
+    print("I AM APPLES!")
+
+# This is just a variable
+tangerine = "Living reflection of a dream"
+
+# This is a Class
+class MyStuff:
+
+    def __init__(self):
+        self.tangerine = "And now a thousand years between"
+
+    def apple(self):
+        print("I AM CLASSY APPLES!")
